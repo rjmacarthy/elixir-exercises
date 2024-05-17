089e931794023cb61ace026f7243a234faa3f787
rjmacarthy <richardmacarthy@hotmail.com>
2024-05-17 12:50:55 +0100

    fix

---
+++ lib/http.ex
@@ -0,0 +1,20 @@
+defmodule Http do
+  use Application
+  @x 10
+
+  def start(_type, _args) do
+    Http.main()
+    Supervisor.start_link(fn -> nil end, name: __MODULE__)
+  end
+
+  def main do
+    case HTTPoison.get("www.google.com") do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        IO.puts body
+      {:ok, %HTTPoison.Response{status_code: 404}} ->
+        IO.puts "Not found :("
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        IO.inspect reason
+    end
+  end
+end
